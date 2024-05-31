5a33d474b0e52634316227f5fb07ab6972c9f000
Galang Priyohutomo <galangpriyohutomo@students.undip.ac.id>
2024-06-01 06:04:56 +0700

    Add file Fragment and function move menu

--- app/src/main/java/com/capstone/emoqu/ui/MainActivity.kt
+++ app/src/main/java/com/capstone/emoqu/ui/MainActivity.kt
@@ -2,11 +2,46 @@ package com.capstone.emoqu.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.capstone.emoqu.R
+import com.capstone.emoqu.ui.fragment.AddFragment
+import com.capstone.emoqu.ui.fragment.ReportFragment
+import com.capstone.emoqu.ui.fragment.TodayFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        bottomNav.setOnNavigationItemSelectedListener { item ->
+            var selectedFragment: Fragment? = null
+            when (item.itemId) {
+                R.id.bottom_today -> {
+                    selectedFragment = TodayFragment()
+                }
+                R.id.fab_add -> {
+                    // Handle the floating action button click here if necessary
+                    // Since it's disabled, this block may not be needed
+                    return@setOnNavigationItemSelectedListener false
+                }
+                R.id.bottom_report -> {
+                    selectedFragment = ReportFragment()
+                }
+            }
+
+            if (selectedFragment != null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit()
+            }
+            true
+        }
+
+        // Set the default selected item
+        bottomNav.selectedItemId = R.id.bottom_today
     }
 }
