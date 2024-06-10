b3c1c302e22c3f91a60f4adaef502f2989fc8e39
Galang Priyohutomo <galangpriyohutomo@students.undip.ac.id>
2024-06-10 22:45:34 +0700

    Supaya Tidak Conflict

--- app/src/main/java/com/capstone/emoqu/ui/add/AddFragment.kt
+++ app/src/main/java/com/capstone/emoqu/ui/add/AddFragment.kt
@@ -1,50 +1,17 @@
-package com.capstone.emoqu.ui.fragment
+package com.capstone.emoqu.ui.add
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.ViewSwitcher
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.capstone.emoqu.R
-
-class TodayFragment : Fragment() {
-
-    private lateinit var viewSwitcher: ViewSwitcher
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var noDataImageView: ImageView
-
+class AddFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_today, container, false)
-
-        viewSwitcher = view.findViewById(R.id.view_switcher)
-        recyclerView = view.findViewById(R.id.rv_follows)
-        noDataImageView = view.findViewById(R.id.iv_no_data)
-
-        // Logika untuk menentukan apakah ada data atau tidak
-        val data = getData()
-        if (data.isEmpty()) {
-            viewSwitcher.displayedChild = 1 // Menampilkan ImageView
-        } else {
-            viewSwitcher.displayedChild = 0 // Menampilkan RecyclerView
-            setupRecyclerView(data)
-        }
-
-        return view
-    }
-
-    private fun getData(): List<Any> {
-        return listOf()
-    }
-
-    private fun setupRecyclerView(data: List<Any>) {
-        recyclerView.layoutManager = LinearLayoutManager(context)
-//        recyclerView.adapter = YourAdapter(data)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add, container, false)
     }
 }
